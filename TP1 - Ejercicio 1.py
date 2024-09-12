7be7a0f79476c806c299693c7ed1d09ff099f966
Nicolás Alfredo Dávalos <nico.vithe15@gmail.com>
2024-09-12 16:18:53 -0300

    Primer ejercicio en python

---
+++ TP1 - Ejercicio 1.py
@@ -0,0 +1,21 @@
+
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+nacimiento = input("Ingrese su fecha de nacimiento: ")
+tituloSecundario = True
+matricula = int(input("Ingrese monto de la matricula: "))
+cuota = matricula + 1000
+pythonI = 12000/4
+print()
+print("====================================================")
+print("======= Uiversidad de Python - Inscripciones =======")
+print("====================================================")
+print()
+print("DATOS DE INGRESO:")
+print("Nombre completo: " + nombre)
+print("Fecha de nacimiento y Edad: " + nacimiento + " (" + edad + ")")
+print(f"Posee título? {tituloSecundario}")
+print(f"Matricula: {matricula}")
+print(f"Cuota Mensual: {cuota}")
+print(f"Aransel mensual materia 'Python I': {pythonI}")
+print(f"Aransel mensual materia 'Python I' con descuento: {pythonI * 0.85}")
